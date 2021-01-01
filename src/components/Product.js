098ff5dc80edbfd0e98f7c182deecff9b4ec4049
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-31 22:13:01 -0800

    Get id in the product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,13 +3,20 @@ import React, { Component } from 'react';
 export class Product extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      addToOrderClicked: false
+    };
   }
   render() {
+    const arr = this.props.productId.history.location.pathname.split('/');
+    const productId = arr[arr.length - 1];
     return (
       <div>
-        Product
-        {this.props.id}
-        {JSON.stringify(this.props.products)}
+        Product productId={productId}
+        {/* {this.props} */}
+        {/* {JSON.stringify(this.props.products)} */}
+        <br></br>
+        <p>hiii</p>
         {JSON.stringify(this.props)}
       </div>
     );
